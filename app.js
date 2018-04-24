b21553f6491defa49fe7195444134acaa5de0733
SeanNoone <seannoone29@gmail.com>
2018-04-24 19:55:11 -0400

    push

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ yText
 // by the Behavioral Risk Factor Surveillance System.
 
 // Import our CSV data with d3's .csv import method.
-d3.csv("assets/data/data.csv", function(data) {
+d3.csv("data.csv", function(data) {
   // Visualize the data
   visualize(data);
 });
